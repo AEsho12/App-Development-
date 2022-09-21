9b3afad37f5ac0cdeb613bf3688e6678f5690ed3
AEsho12 <78968548+AEsho12@users.noreply.github.com>
2022-09-21 15:58:12 +0100

    Add files via upload

---
+++ Dice Roll/Dice Roll/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Dice Roll
+//
+//  Created by Esho, Ayodele on 07/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var label1: UILabel!
+    
+    
+    @IBOutlet weak var info1: UILabel!
+    
+    
+    @IBOutlet weak var Image: UIImageView!
+    
+    @IBAction func DiceRoll(_ sender: Any) {
+    }
+    
+    
+    @IBAction func Buttom1(_ sender: Any) {
+       let diceroll  = Int.random(in: 2..<12)
+        if int =
+    }
+    
+}
+    
+    
+func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
